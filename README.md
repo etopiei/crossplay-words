7e3933600babda94f06dcd0e9ececd6c1a685a5b
etopiei <me.etopiei@gmail.com>
2026-04-27 08:55:17 +1000

    Initial commit.
    Bones of the app works. Still a few things I'd like to change/add but
    it's functional.

---
+++ README.md
@@ -0,0 +1,13 @@
+# crossplay-words
+
+There are a bunch of sites out there for scrabble search/solving but in general they are based around different dictionaries.
+
+This site lets you search through the NYT crossplay dictionary, as well as do 'smart matching' by typing '?' to indicate blank tiles and look for 
+rearrangements of your rack.
+
+## Developing
+
+Requires shadow-cljs installed.
+
+Start with `shadow-cljs watch app`
+
---
+++ src/crossplay_words/core.cljs
@@ -0,0 +1,94 @@
+(ns crossplay-words.core
+  (:require [trie.core :as t]
+            [cljs.pprint]
+            [clojure.string :as str]
+            [clojure.math.combinatorics :as combo]))
+
+(def dict (atom (t/trie [])))
+
+(defn bold-blanks [search-text result]
+  ;; This is actually a little tricky with duplicate chars, maybe need a loop recur that 'uses up' chrs.
+  ;; Maybe come back to this later, for now we don't have to bold blanks.
+  result)
+
+(defn set-results [results search-text]
+  (let [results-string (str/join "<br>" (map #(bold-blanks search-text %) results))]
+  (-> js/document
+    (.getElementById "results")
+    (.-innerHTML)
+    ;; TODO: Consider showing total results found as well as the fact we only display 50.
+    ;; (set! (apply str "50/" (count results) " results displayed<br>" results-string))
+    (set! results-string)
+    )))
+
+(defn exact-match [search-text]
+  (get @dict search-text))
+
+(def a-to-z (map char (range 97 123)))
+
+(defn add-all-az [rack]
+  (map #(str rack %) a-to-z))
+
+(defn append-all [racks appends-left]
+  (if (zero? appends-left)
+    racks
+    (append-all (mapcat add-all-az racks) (dec appends-left))))
+
+(defn get-racks [search-text]
+  (let [non-blank-text (apply str (remove #(= \? %) search-text))
+        num-appends (abs (- (count search-text) (count non-blank-text)))]
+  (append-all [non-blank-text] num-appends)))
+
+(defn rack-permutations [search-text]
+  (let [racks (get-racks search-text)]
+    (map #(apply str %) (mapcat #(combo/permutations %) racks))))
+
+(defn rack-search [search-text]
+  (let [racks (rack-permutations search-text)
+        results (map exact-match racks)]
+    (filter #(not (nil? %)) results)))
+
+(defn handle-rack-search [search-text]
+  (set-results (rack-search search-text) search-text))
+
+(defn handle-exact-search [search-text]
+  (set-results [(exact-match search-text)] search-text))
+
+(defn handle-search [_]
+  (let [search-text (str/lower-case (-> js/document (.getElementById "searchbox") (.-value)))]
+  (if (= "" search-text)
+    (set-results "" [])
+    (let [cheat-mode? (-> js/document
+      (.getElementById "mode-toggle")
+      (.-checked))]
+    (if cheat-mode? (handle-rack-search search-text) (handle-exact-search search-text))))))
+
+(defn set-dict-count []
+  (-> js/document
+    (.getElementById "dict-count")
+    (.-innerHTML)
+    (set! (str "Loaded " (count @dict) " words into dictionary"))))
+
+(defn show-search-box []
+  (-> js/document
+    (.getElementById "search-area")
+    (.setAttribute "style" "display: block")))
+
+(defn on-load []
+  (-> (js/fetch "/words.txt")
+    (.then #(.text %))
+    (.then (fn [word-data]
+      (reset! dict (t/trie (str/split word-data #"\n")))
+      (set-dict-count)
+      (show-search-box))))
+
+  (-> js/document
+    (.getElementById "searchbox")
+    (.addEventListener "input" handle-search))
+
+  (-> js/document
+      (.getElementById "mode-toggle")
+      (.addEventListener "input" handle-search)))
+
+(defn ^:export init []
+  (js/document.addEventListener "DOMContentLoaded" on-load))
